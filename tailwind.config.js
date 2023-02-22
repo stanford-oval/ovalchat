b547346f23db8b1176e46c95a0718de64aabbc76
MasonWang025 <mason.wang0025@gmail.com>
2023-02-22 13:21:02 -0800

    tailwind config file with color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,43 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
+  purge: [
+    "./src/pages/**/*.{js,jsx,ts,tsx}",
+    "./src/components/**/*.{js,jsx,ts,tsx}",
+    ,
+    "./src/data/**/*.{js,jsx,ts,tsx}",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  corePlugins: {
   },
-  plugins: [],
-}
+  theme: {
+    extend: {
+      colors: {
+        factgpt: {
+          primary: {
+            light: "#4779c9",
+            DEFAULT: "#406db6",
+            dark: "#375e9e",
+            dark1: "#224378",
+            dark2: "#152f59",
+          },
+          secondary: {
+            bright: "#287175",
+            light: "#1f676b",
+            DEFAULT: "#194345",
+            dark: "#0d2a2b",
+          },
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {
+      cursor: ["disabled"],
+    },
+  },
+  plugins: [require('@tailwindcss/forms')],
+  style: {
+    postcss: {
+      plugins: [require('tailwindcss'), require('autoprefixer')],
+    },
+  },
+};
