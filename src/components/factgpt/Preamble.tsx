ad02639a50593a71a51cb662564b2fdc296ee370
MasonWang025 <mason.wang0025@gmail.com>
2023-02-23 10:26:03 -0800

    remove unecessary components and update text for preamble

--- src/components/factgpt/Preamble.tsx
+++ src/components/factgpt/Preamble.tsx
@@ -6,24 +6,12 @@ export default function Preamble({ module }: any) {
   return (
     <div className="pt-16">
       <div className="container pt-4 pb-6 md:pt-5 md:pb-8">
-        <Link href="/dashboard#modules">
-          <button className="inline-flex items-center text-gray-700 bg-gray-300 rounded-full p-1 pr-1.5 sm:text-base lg:text-xs hover:text-gray-800">
-            <span className="px-3 py-0.5 text-white text-xs font-semibold leading-5 uppercase tracking-wide bg-factgpt-primary rounded-full">
-              {module.title}
-            </span>
-            <span className="ml-2 text-sm">See all modules</span>
-            <ChevronRightIcon
-              className="ml-0.5 w-5 h-5 text-gray-500"
-              aria-hidden="true"
-            />
-          </button>
-        </Link>
         <h1 className="text-factgpt-secondary font-display text-4xl tracking-tight mt-2">
-          Practice with{" "}
-          <span className="text-factgpt-primary font-medium">Noora</span>
+          Help improve{" "}
+          <span className="text-factgpt-primary font-medium">FactGPT</span>
         </h1>
         <div className="mt-3 text-lg font-base tracking-tight text-gray-700 flex flex-col gap-y-2">
-          <p>{module.selectedDesc}</p>
+          <p>Contribute to FactGPT by picking the better reply option and evaluating replies.</p>
 
           {/* <p>
             For example, if Noora says “
--- src/components/interfaces/noora-chat/menu/sections/Technical.tsx
+++ src/components/interfaces/noora-chat/menu/sections/Technical.tsx
@@ -7,78 +7,7 @@ export default function Technical({ convoState }: any) {
         <span className="font-bold">Current turn: </span>
         {convoState.value.turn}
       </div>
-      
-      <div className="border-gray-300 bg-gray-300 border-1 my-1.5"></div>
-
-      <div className="mt-1 text-gray-700 px-1">
-        <div>
-          <div className="flex items-stretch mt-1">
-            <label htmlFor={"numProblems"} className="font-bold">
-              &#35; Problems:
-            </label>
-            <div className="ml-auto">{convoState.value.numProblems}</div>
-          </div>
-          <input
-            id={"numProblems"}
-            type="range"
-            min={convoState.value.progress.length + 1}
-            max={Math.max(convoState.value.progress.length + 1, 20)}
-            defaultValue={convoState.value.numProblems}
-            disabled={!convoState.value.turn.startsWith("user")}
-            onChange={(e: any) => {
-              convoState.setValue((cs: any) => ({
-                ...cs,
-                numProblems: Number(e.target.value),
-              }));
-            }}
-            step="1"
-            className="w-full h-2 rounded-lg appearance-none cursor-pointer bg-gray-300 gray-slider-thumb"
-          />
-        </div>
-      </div>
-
-      <div className="border-gray-300 bg-gray-300 border-1 my-1.5"></div>
-
       <NoorasMessageDetails convoState={convoState} />
-
-      <Slider
-        parameter={{
-          name: "“Good Reply” Threshold",
-          description: "confidence > threshold → “good reply” rating",
-          min: 0,
-          max: 1,
-          propertyName: "goodReplyThreshold",
-        }}
-        convoState={convoState}
-      />
-      <div className="border-gray-300 bg-gray-300 border-1 my-1.5"></div>
-
-      {[
-        {
-          name: "Temperature",
-          min: 0,
-          max: 1,
-          propertyName: "temperature",
-        },
-        {
-          name: "Frequency Penalty",
-          min: 0,
-          max: 2,
-          propertyName: "frequencyPenalty",
-        },
-        {
-          name: "Presence Penalty",
-          min: 0,
-          max: 2,
-          propertyName: "presencePenalty",
-        },
-      ].map((parameter: any) => (
-        <Slider
-          key={parameter.name}
-          parameter={parameter}
-          convoState={convoState}
-        />
-      ))}
     </div>
   );
 }
@@ -89,6 +18,7 @@ function NoorasMessageDetails({ convoState }: any) {
     convoState.value.progress[convoState.value.progress.length - 1];
   return (
     <div>
+      <div className="border-gray-300 bg-gray-300 border-1 my-1.5"></div>
       <div>
         <b>Problem {convoState.value.progress.length}: </b>
         <span className="font-bold text-factgpt-primary">
@@ -106,8 +36,6 @@ function NoorasMessageDetails({ convoState }: any) {
         )}
         ) confident that the reply was a good one.
       </div>
-
-      <div className="border-gray-300 bg-gray-300 border-1 my-1.5"></div>
     </div>
   );
 }
