d9eb2ca54ef9b056a28579fad51e817cdaa68c21
MasonWang025 <mason.wang0025@gmail.com>
2023-02-23 10:34:37 -0800

    delete unused

--- src/components/factgpt/Preamble.tsx
+++ src/components/factgpt/Preamble.tsx
@@ -1,5 +1,3 @@
-import { ChevronRightIcon } from "@heroicons/react/solid";
-import Link from "next/link";
 import React from "react";
 
 export default function Preamble({ module }: any) {
@@ -12,29 +10,6 @@ export default function Preamble({ module }: any) {
         </h1>
         <div className="mt-3 text-lg font-base tracking-tight text-gray-700 flex flex-col gap-y-2">
           <p>Contribute to FactGPT by picking the best reply option and evaluating replies.</p>
-
-          {/* <p>
-            For example, if Noora says “
-            <b className="text-factgpt-primary special-underline">
-              {module.example.factgpt}
-            </b>
-            ,” a good response might be “
-            <b className="text-factgpt-secondary-light special-underline">
-              {module.example.goodUser}
-            </b>
-            ” <br />
-            <span className="text-gray-500">
-              {module.example.goodExplanation}
-            </span>
-          </p>
-
-          <p>
-            If you replied with “
-            <b className="text-factgpt-secondary-light special-underline">
-              {module.example.badUser}
-            </b>
-            ,” Noora would give you feedback for improving your reply.
-          </p> */}
         </div>
       </div>
     </div>
