e932840f0fcc23646e329c7ec54b4aeaa0716671
GeorgeL <46696710+george1459@users.noreply.github.com>
2023-06-29 16:06:19 -0700

    add two systems

--- src/components/global/branding.tsx
+++ src/components/global/branding.tsx
@@ -8,7 +8,7 @@ export function chatbotName() {
 }
 
 export function allAvailableSystems() {
-    return ["v0616baseline_sql", "v0614baseline_thingtalk"]
+    return ["semantic_index", "baseline_w_textfcns", "baseline_linearization", "v0614baseline_thingtalk"]
 }
 
 export function userScores() {
